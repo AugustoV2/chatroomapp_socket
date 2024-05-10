6d54a7871a6976cfa81b19f6b00155891667518d
AugustoV2 <augustovarghese@gmail.com>
2024-05-10 15:42:38 +0530

    initial stage

---
+++ app/app.js
@@ -0,0 +1,23 @@
+const socket = new WebSocket("ws://localhost:3000")
+
+function sendmessage(e) {
+    e.preventDefault()
+    const input = document.querySelector('input')
+    if (input.value) {
+        socket.send(input.value)
+        input.value = ""
+
+    }
+    input.focus()
+
+}
+
+document.querySelector('form')
+    .addEventListener('submit', sendMessage)
+
+
+socket.addEventListener("message", ({ data }) => {
+    const li = document.createElement('li')
+    li.textContent = data
+    document.querySelector('ul').appendChild(li)
+})
